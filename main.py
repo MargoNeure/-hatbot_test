f7c46d7f2dee14477ee3cad8b6fa989108d9c406
Margarita <kovelkova.m@gmail.com>
2024-06-25 14:17:39 +0300

    добавлены кнопки в тест1

--- main.py
+++ main.py
@@ -2,32 +2,41 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-#from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import config
 
 API_TOKEN = config.token
 
-# Включаем логирование, чтобы видеть сообщения в консоли
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
+# Создаем клавиатуру
+
+def get_keyboard():
+    keyboard = [
+        [KeyboardButton(text="/start")],
+        [KeyboardButton(text="/ура")],
+        [KeyboardButton(text="/info")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
 @dp.message(Command("start"))
 async def send_welcome(message: types.Message):
-    await message.answer("Привет! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.")
+    await message.answer("Привет! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.", reply_markup=get_keyboard())
 
 @dp.message(Command("ура"))
 async def send_ura(message: types.Message):
-    await message.answer("УРАААА! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.")
+    await message.answer("УРАААА! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.", reply_markup=get_keyboard())
+
 @dp.message(Command("info"))
 async def botinfo(message: types.Message):
-    await message.answer("Информация по этому боту не заполнена")
+    await message.answer("Информация по этому боту не заполнена", reply_markup=get_keyboard())
 
 @dp.message()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer(message.text, reply_markup=get_keyboard())
 
 async def main():
     await dp.start_polling(bot)
