decf3235e0b18681eb526e3f257a228c43fa9c04
Margarita <kovelkova.m@gmail.com>
2024-06-25 22:46:14 +0300

    добавлен 1 сценарий в тест1

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import logging
+import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-import asyncio
-
+import config
 
 API_TOKEN = config.token
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
+
+
+# Создаем клавиатуру
+def get_keyboard():
+    keyboard = [
+        [KeyboardButton(text="Поприветствовать")],
+        [KeyboardButton(text="Крикнуть 'Ура!'")],
+        [KeyboardButton(text="Информация о боте")],
+        [KeyboardButton(text="Выбрать ручку")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
 
 
 class ChoosePen(StatesGroup):
@@ -22,29 +34,41 @@ class ChoosePen(StatesGroup):
     waiting_for_line_thickness = State()
 
 
-@dp.message(Command(commands=['start', 'help']))
+@dp.message(lambda message: message.text == "Поприветствовать")
 async def send_welcome(message: types.Message):
-    await message.reply("Привет! Я помогу вам выбрать идеальную ручку. Начнем? Напишите /choose")
+    await message.answer("Привет! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.",
+                         reply_markup=get_keyboard())
 
 
-@dp.message(Command("choose"))
-async def cmd_choose(message: types.Message, state: FSMContext):
+@dp.message(lambda message: message.text == "Крикнуть 'Ура!'")
+async def send_ura(message: types.Message):
+    await message.answer("УРАААА! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.",
+                         reply_markup=get_keyboard())
+
+
+@dp.message(lambda message: message.text == "Информация о боте")
+async def botinfo(message: types.Message):
+    await message.answer("Информация по этому боту не заполнена", reply_markup=get_keyboard())
+
+
+@dp.message(lambda message: message.text == "Выбрать ручку")
+async def start_choose_pen(message: types.Message, state: FSMContext):
     await state.set_state(ChoosePen.waiting_for_purpose)
-    await message.reply("Для каких целей вам нужна ручка? (работа/учеба/творчество)")
+    await message.answer("Для каких целей вам нужна ручка? (работа/учеба/творчество)")
 
 
 @dp.message(ChoosePen.waiting_for_purpose)
 async def process_purpose(message: types.Message, state: FSMContext):
     await state.update_data(purpose=message.text.lower())
     await state.set_state(ChoosePen.waiting_for_ink_color)
-    await message.reply("Какой цвет чернил вы предпочитаете? (синий/черный/красный)")
+    await message.answer("Какой цвет чернил вы предпочитаете? (синий/черный/красный)")
 
 
 @dp.message(ChoosePen.waiting_for_ink_color)
 async def process_ink_color(message: types.Message, state: FSMContext):
     await state.update_data(ink_color=message.text.lower())
     await state.set_state(ChoosePen.waiting_for_line_thickness)
-    await message.reply("Какую толщину линии вы предпочитаете? (тонкая/средняя/толстая)")
+    await message.answer("Какую толщину линии вы предпочитаете? (тонкая/средняя/толстая)")
 
 
 @dp.message(ChoosePen.waiting_for_line_thickness)
@@ -64,14 +88,15 @@ async def process_line_thickness(message: types.Message, state: FSMContext):
         "Стандартная модель 'Универсал': подходит для различных задач"
     )
 
-    await message.reply(
-        f"На основе ваших предпочтений, я рекомендую следующую ручку:\n{recommendation}\n\nЕсли вы хотите узнать о других моделях или у вас есть дополнительные вопросы, пожалуйста, дайте мне знать.")
+    await message.answer(
+        f"На основе ваших предпочтений, я рекомендую следующую ручку:\n{recommendation}\n\nЕсли вы хотите узнать о других моделях или у вас есть дополнительные вопросы, пожалуйста, дайте мне знать.",
+        reply_markup=get_keyboard())
     await state.clear()
 
 
 @dp.message()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer(message.text, reply_markup=get_keyboard())
 
 
 async def main():
