0524afdc9b2b541aefe325bb81be5788f3027fd6
Margarita <kovelkova.m@gmail.com>
2024-06-28 00:21:36 +0300

    настройка картинок начало в test2

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 import config
+from unsplash.api import Api
+from unsplash.auth import Auth
 
 API_TOKEN = config.token
 
@@ -16,33 +18,39 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
+auth = Auth('YOUR_ACCESS_KEY', 'YOUR_SECRET_KEY', 'YOUR_REDIRECT_URI')
+api = Api(auth)
 
-# Создаем клавиатуру
-def get_keyboard():
+# Создаем начальную клавиатуру
+def get_start_keyboard():
+    keyboard = [[KeyboardButton(text="Поприветствовать")]]
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
+# Создаем основную клавиатуру
+def get_main_keyboard():
     keyboard = [
-        [KeyboardButton(text="Поприветствовать")],
         [KeyboardButton(text="Информация о продукции")],
         [KeyboardButton(text="Информация о боте")],
         [KeyboardButton(text="Выбрать ручку")]
     ]
     return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
 
-
 class ChoosePen(StatesGroup):
     waiting_for_purpose = State()
     waiting_for_ink_color = State()
     waiting_for_line_thickness = State()
 
-
 class ProductInfo(StatesGroup):
     waiting_for_category = State()
     waiting_for_model = State()
 
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Добро пожаловать! Нажмите кнопку 'Поприветствовать', чтобы начать.", reply_markup=get_start_keyboard())
 
 @dp.message(lambda message: message.text == "Поприветствовать")
 async def send_welcome(message: types.Message):
-    await message.answer("Привет! Я бот-помощник по выбору ручек. Чем могу помочь?", reply_markup=get_keyboard())
-
+    await message.answer("Привет! Я бот-помощник по выбору ручек. Чем могу помочь?", reply_markup=get_main_keyboard())
 
 @dp.message(lambda message: message.text == "Информация о продукции")
 async def start_product_info(message: types.Message, state: FSMContext):
@@ -53,7 +61,6 @@ async def start_product_info(message: types.Message, state: FSMContext):
         [InlineKeyboardButton(text="Campus Vibes (студенческая серия)", callback_data="category_economy")]
     ])
     await message.answer("Выберите категорию ручек:", reply_markup=keyboard)
-
 
 @dp.callback_query(lambda c: c.data.startswith('category_'))
 async def process_category(callback_query: types.CallbackQuery, state: FSMContext):
@@ -81,7 +88,6 @@ async def process_category(callback_query: types.CallbackQuery, state: FSMContex
     await callback_query.message.edit_text(f"Вы выбрали категорию: {category_name}\nВыберите модель ручки:",
                                            reply_markup=keyboard)
 
-
 @dp.callback_query(lambda c: c.data.startswith('model_'))
 async def process_model(callback_query: types.CallbackQuery, state: FSMContext):
     model = callback_query.data.split('_')[1]
@@ -90,24 +96,15 @@ async def process_model(callback_query: types.CallbackQuery, state: FSMContext):
     category_name = data['category_name']
 
     pen_info = {
-        ("business",
-         "руководитель"): "Модель Ювелирная грация: Утонченный корпус украшен замысловатым растительным узором, тщательно вырезанным вручную мастерами своего дела. http://penatelier.tilda.ws/tproduct/475671006932-yuvelirnaya-gratsiya",
-        ("business",
-         "профессионал"): "Модель Хрустальная фантазия: Ее уникальный корпус, покрытый мерцающими кристаллами, переливается нежными оттенками розового и лилового, напоминая сказочные сумерки или цветущие поля лаванды. http://penatelier.tilda.ws/tproduct/262098745112-hrustalnaya-fantaziya",
-        ("business",
-         "дипломат"): "Модель Изумрудный Элеганс: Элегантный зеленый корпус этой ручки привлекает внимание своей глубиной и насыщенностью. Серебряные детали придают ей утонченный и дорогой вид. http://penatelier.tilda.ws/tproduct/570192768822-izumrudnii-elegans",
-        ("economy",
-         "студент"): "Модель Фламинго Блюз: Эта роскошная ручка ручной работы излучает роскошь и яркий стиль. http://penatelier.tilda.ws/tproduct/436039085332-flamingo-blyuz",
-        ("economy",
-         "начинающий писатель"): "Модель Гармония Огонь и Лед: Ярко-оранжевый корпус в сочетании с глубоким синим оттенком делает ее заметной и стильной. Металлические детали придают ей элегантность и прочность. http://penatelier.tilda.ws/tproduct/467939644822-garmoniya-ogon-i-led",
-        ("economy",
-         "начинающий дизайнер"): "Модель Basic: простая и надежная ручка для офисной работы. Экономичный выбор для больших организаций.",
-        ("designer",
-         "дизайнер"): "Модель Калейдоскоп красок: Корпус покрыт ярким калейдоскопом цветов и форм, создающих неповторимый узор, полный жизни и энергии. http://penatelier.tilda.ws/tproduct/696281938322-kaleidoskop-krasok",
-        ("designer",
-         "писатель"): "Модель Вихрь Вдохновения:  Многоцветные спирали на корпусе напоминают о бесконечности креативных идей. Золотые акценты добавляют нотку роскоши и утонченности. http://penatelier.tilda.ws/tproduct/509816028762-vihr-vdohnoveniya",
-        ("designer",
-         "художник"): "Модель Special: уникальная ручка с необычным дизайном. Прекрасный подарок для творческих людей."
+        ("business", "руководитель"): "Модель Ювелирная грация: Утонченный корпус украшен замысловатым растительным узором, тщательно вырезанным вручную мастерами своего дела. http://penatelier.tilda.ws/tproduct/475671006932-yuvelirnaya-gratsiya",
+        ("business", "профессионал"): "Модель Хрустальная фантазия: Ее уникальный корпус, покрытый мерцающими кристаллами, переливается нежными оттенками розового и лилового, напоминая сказочные сумерки или цветущие поля лаванды. http://penatelier.tilda.ws/tproduct/262098745112-hrustalnaya-fantaziya",
+        ("business", "дипломат"): "Модель Изумрудный Элеганс: Элегантный зеленый корпус этой ручки привлекает внимание своей глубиной и насыщенностью. Серебряные детали придают ей утонченный и дорогой вид. http://penatelier.tilda.ws/tproduct/570192768822-izumrudnii-elegans",
+        ("economy", "студент"): "Модель Фламинго Блюз: Эта роскошная ручка ручной работы излучает роскошь и яркий стиль. http://penatelier.tilda.ws/tproduct/436039085332-flamingo-blyuz",
+        ("economy", "начинающий писатель"): "Модель Гармония Огонь и Лед: Ярко-оранжевый корпус в сочетании с глубоким синим оттенком делает ее заметной и стильной. Металлические детали придают ей элегантность и прочность. http://penatelier.tilda.ws/tproduct/467939644822-garmoniya-ogon-i-led",
+        ("economy", "начинающий дизайнер"): "Модель Basic: простая и надежная ручка для офисной работы. Экономичный выбор для больших организаций.",
+        ("designer", "дизайнер"): "Модель Калейдоскоп красок: Корпус покрыт ярким калейдоскопом цветов и форм, создающих неповторимый узор, полный жизни и энергии. http://penatelier.tilda.ws/tproduct/696281938322-kaleidoskop-krasok",
+        ("designer", "писатель"): "Модель Вихрь Вдохновения:  Многоцветные спирали на корпусе напоминают о бесконечности креативных идей. Золотые акценты добавляют нотку роскоши и утонченности. http://penatelier.tilda.ws/tproduct/509816028762-vihr-vdohnoveniya",
+        ("designer", "художник"): "Модель Special: уникальная ручка с необычным дизайном. Прекрасный подарок для творческих людей."
     }
 
     info = pen_info.get((category, model), "Информация о данной модели отсутствует.")
@@ -120,8 +117,7 @@ async def process_model(callback_query: types.CallbackQuery, state: FSMContext):
 async def botinfo(message: types.Message):
     await message.answer(
         "Я бот-помощник по выбору ручек. Я могу помочь вам выбрать идеальную ручку или предоставить информацию о нашей продукции.",
-        reply_markup=get_keyboard())
-
+        reply_markup=get_main_keyboard())
 
 @dp.message(lambda message: message.text == "Выбрать ручку")
 async def start_choose_pen(message: types.Message, state: FSMContext):
@@ -132,7 +128,6 @@ async def start_choose_pen(message: types.Message, state: FSMContext):
         [InlineKeyboardButton(text="Творчество", callback_data="purpose_творчество")]
     ])
     await message.answer("Для каких целей вам нужна ручка?", reply_markup=keyboard)
-
 
 @dp.callback_query(lambda c: c.data.startswith('purpose_'))
 async def process_purpose(callback_query: types.CallbackQuery, state: FSMContext):
@@ -146,7 +141,6 @@ async def process_purpose(callback_query: types.CallbackQuery, state: FSMContext
     ])
     await callback_query.message.edit_text(f"Вы выбрали цель: {purpose}\n\nКакой цвет чернил вы предпочитаете?",
                                            reply_markup=keyboard)
-
 
 @dp.callback_query(lambda c: c.data.startswith('color_'))
 async def process_ink_color(callback_query: types.CallbackQuery, state: FSMContext):
@@ -162,7 +156,6 @@ async def process_ink_color(callback_query: types.CallbackQuery, state: FSMConte
     await callback_query.message.edit_text(
         f"Вы выбрали цель: {data['purpose']}\nЦвет чернил: {color}\n\nКакую толщину линии вы предпочитаете?",
         reply_markup=keyboard)
-
 
 @dp.callback_query(lambda c: c.data.startswith('thickness_'))
 async def process_line_thickness(callback_query: types.CallbackQuery, state: FSMContext):
@@ -189,17 +182,14 @@ async def process_line_thickness(callback_query: types.CallbackQuery, state: FSM
     await callback_query.message.edit_text(result_text)
     await state.clear()
 
-
 @dp.message()
 async def echo(message: types.Message):
     await message.answer(
         "Извините, я не понял ваш запрос. Пожалуйста, воспользуйтесь кнопками меню для выбора действия.",
-        reply_markup=get_keyboard())
-
+        reply_markup=get_main_keyboard())
 
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == '__main__':
     asyncio.run(main())
