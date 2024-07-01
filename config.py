00511b85fa561e15f01a7d945b7af88bbb4e295a
MargoNeure <neuromouse.m@gmail.com>
2024-07-01 14:38:14 +0300

    удалены ключи для приватности

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = "7378216302:AAFyxP4_nPNJx7_O_CMygV0kxQO0mQ7iV8s"
-key1 = "yClbk2wsSbgx0ein8r5R6Sep7gitarrvGus5Rq9QkIA"
-key2 = "itAcdoXaOd20u6JDRgyfY8N1k8sef9ALL4IgJSJrnYI"
-key3 = "622581"
+token = "удален"
+key1 = "удален"
+key2 = "удален"
+key3 = "удален"
